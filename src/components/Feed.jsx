c70c98942e489caf74c816fa9203d1fc16b897b9
ak-0000 <atulbhakra@gmail.com>
2024-10-11 00:46:12 +0530

    signup

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,31 +6,41 @@ import { addFeed } from "../utils/feedSlice";
 import UserCard from "./UserCard";
 
 const Feed = () => {
-  const feed = useSelector((store) => store.feed);
-  // console.log(feed);
+  const feed = useSelector((store) => store.feed); // Fetching feed from Redux store
   const dispatch = useDispatch();
+
   const getFeed = async () => {
-    if (feed) return;
+    // Fetch feed only if it's not already loaded
+    if (feed && feed.length > 0) return;
+    
     try {
       const res = await axios.get(BASE_URL + "/feed", {
         withCredentials: true,
       });
-      dispatch(addFeed(res?.data));
-    } catch (err) {}
+      dispatch(addFeed(res?.data || [])); // Dispatching fetched data to store, or empty array if no data
+    } catch (err) {
+      console.error("Error fetching feed", err);
+    }
   };
 
   useEffect(() => {
     getFeed();
-  }, []);
-  if (feed)
-    return <h1 className=" flex justify-center mt-20 text-bold text-white text-lg">no more users</h1>;
+  }, []); // Fetch feed on component mount
 
+  // Handling the case where there are no users in the feed
+  if (!feed || feed.length === 0) {
+    return (
+      <h1 className="flex justify-center mt-20 text-bold text-white text-lg">
+        No more users
+      </h1>
+    );
+  }
+
+  // Rendering user cards when feed has data
   return (
-    feed && (
-      <div className="flex justify-center my-10">
-        <UserCard user={feed[0]} />
-      </div>
-    )
+    <div className="flex justify-center my-10">
+      <UserCard user={feed[0]} />
+    </div>
   );
 };
 
